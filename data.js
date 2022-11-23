d99012c90cc07bbf91f438cf151c7e14503944f6
Anitha-najani <anithanajani26@gmail.com>
2022-11-23 12:00:28 +0530

    express

---
+++ data.js
@@ -0,0 +1,6 @@
+const students=[
+      {stdid:1,stdname:"lalli",stdclass:"mpcs"},
+       {stdid:2,stdname:"manasa",stdclass:"mpc"},
+        {stdid:3,stdname:"navya",stdclass:"mecs"},
+]
+module.exports=students
